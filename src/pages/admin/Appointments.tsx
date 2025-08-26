a3e7ddbceb16c2ecc3bacc024153d3c32549b8d4
Domingos André <domandreqs@gmail.com>
2025-08-26 18:42:09 +0100

    first commit dap-2-admin

--- src/pages/admin/Appointments.tsx
+++ src/pages/admin/Appointments.tsx
@@ -2,7 +2,7 @@ export default function Appointments() {
   return (
     <div className="p-6">
       <h1 className="text-xl font-semibold">Agendamentos</h1>
-      <p>Core dos agendamentos dos pacientes</p>
+      <p>Page Core dos agendamentos dos pacientes</p>
     </div>
   );
 }
